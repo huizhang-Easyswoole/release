5a8bca9db8678f79e8cecdb71120f7f0d5591472
huizhang001 <tuzisir@163.com>
2020-04-04 15:06:17 +0800

    up

--- App/Relase/Relase.php
+++ App/Relase/Relase.php
@@ -10,11 +10,14 @@ class Relase extends AbstractProcess
         go(static function () {
             while (true)
             {
+                echo '开始Git pull'.PHP_EOL;
                 $exec = 'cd ' .EASYSWOOLE_ROOT. '; git pull';
                 $pullResult = exec($exec);
-                var_dump($pullResult);
-                if ($pullResult !== 'Already up to date.') {
+                if (strpos($pullResult, 'Unpacking objects: 100%') !== false) {
+                    echo '有新版本发布'.PHP_EOL;
 
+                } else {
+                    echo '无新版本发布'.PHP_EOL;
                 }
                 // 每5秒同步一次代码
                 Coroutine::sleep(5);
