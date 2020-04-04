d3086b8e4e4a1b21a7429535beee0f597e58eb05
huizhang001 <tuzisir@163.com>
2020-04-04 17:19:08 +0800

    up

--- App/Relase/Relase.php
+++ App/Relase/Relase.php
@@ -10,32 +10,54 @@ class Relase extends AbstractProcess
         go(static function () {
             while (true)
             {
-                echo '开始检测是否有新提交'.PHP_EOL;
+                Coroutine::sleep(5);
 
                 $diffExec = 'cd ' .EASYSWOOLE_ROOT. '; git fetch; git diff --stat master origin/master;';
                 $pullResult = exec($diffExec);
 
-                if (trim($pullResult) !== '') {
-                    echo '有新版本发布'.PHP_EOL;
-                    $cloneExec = 'git clone https://github.com/huizhang-Easyswoole/release.git ../relase-'.date('Y-m-d H:i:s');
-                    $cloneResult = exec($cloneExec);
-                    var_dump($cloneResult);
+                if ($pullResult !== '') {
+                    $newVersionPath = '../relase-'.time();
+
+                    $cloneExec = "git clone https://github.com/huizhang-Easyswoole/release.git {$newVersionPath};cd {$newVersionPath};composer update;";
+                    exec($cloneExec);
+
+                    $lsofExec = 'lsof -i:9501';
+                    $lsofResult = exec($lsofExec);
+                    $newPort = 9501;
+                    $oldPort = 9502;
+                    if ($lsofResult !== '') {
+                        $newPort = 9502;
+                        $oldPort = 9501;
+                    }
+
+                    echo '开始替换启动端口'.PHP_EOL;
+                    $devConfig = file_get_contents($newVersionPath.'/dev.php');
+                    $devConfig = str_replace($oldPort, $newPort, $devConfig);
+                    file_put_contents($newVersionPath.'/dev.php', $devConfig);
+
+                    $startExec = "cd {$newVersionPath}; php easyswoole start d";
+                    exec($startExec);
+
+                    // 替换nginx配置
+                    $ngConfigPath = '/usr/local/etc/nginx/nginx.conf';
+                    $ngConfig  = file_get_contents($ngConfigPath);
+                    $ngConfig = str_replace($oldPort, $newPort, $ngConfig);
+                    file_put_contents($ngConfigPath, $ngConfig);
+
+                    // 重启nginx
+                    $reloadNgExec = 'nginx -s reload';
+                    exec($reloadNgExec);
+
+                    $stopExec = 'php easyswoole stop';
+                    exec($stopExec);
+
+                    // 每5秒同步一次代码1
+                    Coroutine::sleep(100);
                 } else {
                     echo '无新版本发布'.PHP_EOL;
 
                 }
 
-//                $exec = 'cd ' .EASYSWOOLE_ROOT. '; git pull';
-//                $pullResult = exec($exec);
-//
-//                if (strpos($pullResult, 'Unpacking objects: 100%') !== false) {
-//                    echo '有新版本发布'.PHP_EOL;
-//
-//                } else {
-//                    echo '无新版本发布'.PHP_EOL;
-//                }
-                // 每5秒同步一次代码1
-                Coroutine::sleep(5);
             }
         });
 
