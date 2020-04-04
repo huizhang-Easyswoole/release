497b5c2be04488adf34ece4f9bba754858dd3e7c
huizhang001 <tuzisir@163.com>
2020-04-04 15:27:19 +0800

    up

--- App/Relase/Relase.php
+++ App/Relase/Relase.php
@@ -34,7 +34,7 @@ class Relase extends AbstractProcess
 //                } else {
 //                    echo '无新版本发布'.PHP_EOL;
 //                }
-                // 每5秒同步一次代码1
+                // 每5秒同步一次代码
                 Coroutine::sleep(5);
             }
         });
